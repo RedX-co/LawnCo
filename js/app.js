a1c60ff3a74668cc7ab6a10f0b1d3b7e8176c583
upliftsleet1974 <upliftsleet1974@gmail.com>
2015-02-09 15:15:30 -0700

    no

--- js/app.js
+++ js/app.js
@@ -12,12 +12,3 @@ app.get('/', function(req, res){
         });
 
 });
-
-var swig = require('swig');
-
-// Compile a file and store it, rendering it later
-var tpl = swig.compileFile('/path/to/template.html');
-console.log(tpl({ article: { title: 'Swig is fun!' }}));
-
-// Immediately render a Swig template from a string
-console.log(swig.render('{% if foo %}Hooray!{% endif %}', { locals: { foo: true }}));
